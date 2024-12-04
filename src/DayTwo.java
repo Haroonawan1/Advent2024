2f8aa3ec96c595af5b90fec46ce175852d1682f1
Haroon Awan <haroona13@nycstudents.net>
2024-12-04 13:18:02 -0500

    sad code

--- src/DayTwo.java
+++ src/DayTwo.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class DayTwo {
@@ -38,18 +39,25 @@ public class DayTwo {
 
         for (String fileDatum : fileData) {
             String[] splitData = fileDatum.split(" ");
-            boolean valid = true;
-            int errorCount = 0;
-            int initialDelta = Integer.parseInt(splitData[0]) - Integer.parseInt(splitData[1]);
+            ArrayList<Integer> deltas = new ArrayList<>(List.of(splitData.length - 1));
 
             for (int j = 0; j < splitData.length - 1; j++) {
-                int delta = Integer.parseInt(splitData[j]) - Integer.parseInt(splitData[j + 1]);
-                if (!(1 <= Math.abs(delta) && Math.abs(delta) <= 3 && (initialDelta * delta) > 0)) {
-                    errorCount++;
+                deltas.add(Integer.parseInt(splitData[j]) - Integer.parseInt(splitData[j + 1]));
+            }
+
+            boolean valid = true;
+            for (int j = 0; j < deltas.size() - 1; j++) {
+                if (!(1 <= Math.abs(deltas.get(j)) && Math.abs(deltas.get(j)) <= 3 && (deltas.get(j) * deltas.get(j + 1) > 0))) {
+                    int currentRemoved = deltas.remove(j);
+                    for (int k = 0; k < deltas.size() - 1; k++) {
+                        if (!(1 <= Math.abs(deltas.get(k)) && Math.abs(deltas.get(k)) <= 3 && (deltas.get(k) * deltas.get(k + 1) > 0))) {
+                            valid = false;
+                        }
+                    }
                 }
             }
 
-            if (errorCount <= 1) {
+            if (valid) {
                 validReports++;
             }
         }
