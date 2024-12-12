a3eaef8a80b8bf5255ac9b2f760f49e8774b25ee
Haroon Awan <haroona13@nycstudents.net>
2024-12-12 13:16:58 -0500

    Almost finished day 4 part 2, part 1 done

--- src/DayFour.java
+++ src/DayFour.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class DayFour {
     public static void main(String[] args) {
-        ArrayList<String> fileData = getFileData("data/testInput");
+        ArrayList<String> fileData = getFileData("data/day4Input");
         System.out.println("Star One: " + starOne(fileData));
         System.out.println("Star Two: " + starTwo(fileData));
     }
@@ -22,12 +22,16 @@ public class DayFour {
         for (int i = 0; i < data.length; i++) {
             for (int j = 0; j < data[i].length; j++) {
                 if (data[i][j].equals("X")) {
-                    boolean[] checks = new boolean[8];
-                    checks[0] = checkLeftRight(data[i], j, "left");
-                    checks[1] = checkLeftRight(data[i], j, "right");
-                    checks[2] = checkUpDown(data, i, j, "up");
-                    checks[3] = checkUpDown(data, i, j, "down");
-
+                    boolean[] checks = {
+                            checkLeftRight(data[i], j, "left"),
+                            checkLeftRight(data[i], j, "right"),
+                            checkUpDown(data, i, j, "up"),
+                            checkUpDown(data, i, j, "down"),
+                            checkDiags(data, i, j, "topRight"),
+                            checkDiags(data, i, j, "topLeft"),
+                            checkDiags(data, i, j, "bottomRight"),
+                            checkDiags(data, i, j, "bottomLeft")
+                    };
 
                     for (boolean check : checks) {
                         if (check) {
@@ -68,24 +72,66 @@ public class DayFour {
         return check.equals("XMAS");
     }
 
-    public static boolean checkDiags(String[][] data, int row, int col) {
+    public static boolean checkDiags(String[][] data, int row, int col, String direction) {
+        int rowModifier = 1;
+        int colModifier = 1;
 
-        //based on direction multiply by neg
+        switch (direction) {
+            case "topRight" -> rowModifier *= -1;
+            case "topLeft" -> {
+                rowModifier *= -1;
+                colModifier *= -1;
+            }
+            case "bottomLeft" -> colModifier *= -1;
+        }
 
         String check = "";
         try {
             for (int i = 0; i < 4; i++) {
-                check += data[row + i][col + i];
+                check += data[row + (i * rowModifier)][col + (i * colModifier)];
             }
         } catch (Exception _) {}
-
-
 
         return check.equals("XMAS");
     }
 
     public static int starTwo(ArrayList<String> fileData) {
-        return 0;
+        String[][] data = new String[fileData.size()][fileData.getFirst().length()];
+        for (int i = 0; i < fileData.size(); i++) {
+            data[i] = fileData.get(i).split("");
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[i].length; j++) {
+
+                if ("MS".contains(data[i][j])) {
+                    String check = "";
+                    try {
+                        for (int k = 0; k < 3; k++) {
+                            for (int l = 0; l < 3; l++) {
+                                if ((k % 2 == 0 && l % 2 == 0) || (k % 2 != 0 && l % 2 != 0)) {
+                                    check += data[i + k][j + l];
+                                }
+                            }
+                        }
+
+                        ArrayList<String> checkArr = new ArrayList<>(Arrays.asList(check.split("")));
+                        checkArr.sort(String.CASE_INSENSITIVE_ORDER);
+                        check = String.join("", checkArr);
+
+                        if (check.equals("AMMSS")) {
+                            sum++;
+                        }
+
+                        //System.out.println(check);
+                    } catch (Exception _) {}
+                }
+            }
+        }
+
+        return sum;
     }
 
     public static ArrayList<String> getFileData(String fileName) {
