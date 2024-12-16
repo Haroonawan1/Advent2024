00d25fa5d97ebc4a9c819a7741bd1c8e5358d9c9
Haroon Awan <haroona13@nycstudents.net>
2024-12-16 13:18:34 -0500

    day 5 almost done

--- src/DayFive.java
+++ src/DayFive.java
@@ -23,16 +23,64 @@ public class DayFive {
             }
         }
 
-        for (int i = 0; i < pages.size(); i++) {
-            String[] splitPage = pages.get(i).split(",");
-            System.out.println(splitPage[0]);
+        int sum = 0;
+        for (String page : pages) {
+            ArrayList<String> splitPage = new ArrayList<>(Arrays.asList(page.split(",")));
+            boolean valid = isValid(splitPage, rules);
+
+            if (valid) {
+                sum += Integer.parseInt(splitPage.get(splitPage.size() / 2));
+            }
         }
 
-        return 0;
+        return sum;
     }
 
     public static int starTwo(ArrayList<String> fileData) {
-        return 0;
+        ArrayList<String> rules = new ArrayList<>();
+        ArrayList<String> pages = new ArrayList<>();
+        //ArrayList<String[]> orderedInvalids = new ArrayList<>();
+
+        for (String dataRow : fileData) {
+            if (dataRow.contains("|")) {
+                rules.add(dataRow);
+            } else {
+                pages.add(dataRow);
+            }
+        }
+
+        int sum = 0;
+        for (int i = 0; i < pages.size(); i++) {
+            ArrayList<String> splitPage = new ArrayList<>(Arrays.asList(pages.get(i).split(",")));
+            boolean valid = isValid(splitPage, rules);
+
+            if (!valid) {
+                String[] orderedInvalid = new String[splitPage.size()];
+                //put in a num, check if valid
+                //if invalid move latest num back one index until list is valid
+                //repeat
+
+                sum += Integer.parseInt(orderedInvalid[orderedInvalid.length / 2]);
+            }
+        }
+
+        return sum;
+    }
+
+    public static boolean isValid(ArrayList<String> splitPage, ArrayList<String> rules) {
+        boolean valid = true;
+
+        for (int j = 0; j < splitPage.size(); j++) {
+            for (int k = 0; k < rules.size(); k++) {
+                String firstNum = rules.get(k).substring(0, 2);
+                String secondNum = rules.get(k).substring(3);
+
+                if (splitPage.get(j).equals(firstNum) && splitPage.contains(secondNum) && j > splitPage.indexOf(secondNum)) {
+                    valid = false;
+                }
+            }
+        }
+        return valid;
     }
 
     public static ArrayList<String> getFileData(String fileName) {
